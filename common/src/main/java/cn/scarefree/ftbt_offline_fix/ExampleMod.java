81315df1e6351492c391a323cf679b0cd8138907
Terry-MC123 <m13884967603@163.com>
2025-03-23 16:28:36 +0800

    Init

---
+++ common/src/main/java/cn/scarefree/ftbt_offline_fix/ExampleMod.java
@@ -0,0 +1,9 @@
+package cn.scarefree.ftbt_offline_fix;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "ftbt_offline_fix";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
