f08e6d83e98613531323a41f331403c26310b8f7
Terry-MC123 <terry_mc@qq.com>
2025-05-15 19:23:28 +0800

    fix

--- common/src/main/java/cn/scarefree/ftbt_offline_fix/mixin/ClientTeamManagerMixin.java
+++ common/src/main/java/cn/scarefree/ftbt_offline_fix/mixin/ClientTeamManagerMixin.java
@@ -1,26 +1,41 @@
 package cn.scarefree.ftbt_offline_fix.mixin;
 
+import dev.ftb.mods.ftbteams.FTBTeams;
 import dev.ftb.mods.ftbteams.data.ClientTeam;
 import dev.ftb.mods.ftbteams.data.ClientTeamManager;
 import dev.ftb.mods.ftbteams.data.KnownClientPlayer;
-import dev.ftb.mods.ftbteams.data.TeamManager;
 import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.UUID;
 
 @Mixin(ClientTeamManager.class)
-public class ClientTeamManagerMixin {
+public abstract class ClientTeamManagerMixin {
+    @Shadow(remap = false) public ClientTeam selfTeam;
+
+    @Shadow(remap = false) @Final public Map<UUID, ClientTeam> teamMap;
+
     @Shadow(remap = false) public KnownClientPlayer selfKnownPlayer;
 
-    @Inject(method = "initSelfDetails", at = @At("TAIL"), remap = false)
+    @Shadow(remap = false) @Final public Map<UUID, KnownClientPlayer> knownPlayers;
+
+    @Inject(method = "initSelfDetails", at = @At("HEAD"), cancellable = true, remap = false)
     public void initSelfDetails(UUID selfTeamID, CallbackInfo ci) {
-        if(Minecraft.getInstance().getUser().getGameProfile().isLegacy()){
-            selfKnownPlayer=new KnownClientPlayer(TeamManager.INSTANCE.getInternalPlayerTeam(Minecraft.getInstance().getUser().getGameProfile().getId()));
+        this.selfTeam = this.teamMap.get(selfTeamID);
+        String playerName = Minecraft.getInstance().getUser().getName();
+        UUID playerId = UUID.nameUUIDFromBytes(("OfflinePlayer:" + playerName).getBytes(StandardCharsets.UTF_8));
+        this.selfKnownPlayer = knownPlayers.get(playerId);
+        if (selfKnownPlayer == null) {
+            FTBTeams.LOGGER.warn("Local player id {} was not found in the known players list [{}]! FTB Teams will not be able to function correctly!",
+                    playerId, String.join(",", knownPlayers.keySet().stream().map(UUID::toString).toList()));
         }
+        ci.cancel();
     }
 }
